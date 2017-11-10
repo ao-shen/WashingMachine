0b353e54e0873835d6927219d0b39d1d526b6f48
ao-shen <ao.shen@gmail.com>
2017-11-09 22:54:12 -0500

    First

---
+++ Source/WashingMachine/WashingMachineGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WashingMachineGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWashingMachineGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AWashingMachineGameMode();
+};
+
+
+
